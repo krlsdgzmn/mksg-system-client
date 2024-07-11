b27c97553c0ee2251109a783ca6f443f46683ce0
krlsdgzmn <johnkarlos.deguzman.d@gmail.com>
2024-07-11 17:50:14 +0800

    + Added OrderForecastType

---
+++ types/index.ts
@@ -0,0 +1,10 @@
+export type OrderForecastType = {
+  id: number;
+  price_bin: string;
+  discount_bin: string;
+  month: string;
+  week: number;
+  distance_bin: string;
+  cancel_rate: number;
+  order_status: string;
+};
