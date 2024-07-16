a16c99b3a408e889c8c8b62cf5d4bde96e231944
krlsdgzmn <johnkarlos.deguzman.d@gmail.com>
2024-07-16 14:49:47 +0800

    + Added custom hooks for order forecast

---
+++ app/order-forecast/hooks.ts
@@ -0,0 +1,25 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { getFilteredOrderForecast, getOrderForecast } from "./actions";
+import { OrderForecast } from "./types";
+
+const QUERY_KEY = "order-forecast";
+
+// Custom hook to fetch order forecasts without filters
+export const useGetOrderForecast = () => {
+  return useQuery<OrderForecast[]>({
+    queryKey: [QUERY_KEY],
+    queryFn: getOrderForecast,
+    staleTime: 1000 * 60 * 1,
+  });
+};
+
+// Custom hook to fetch order forecasts with filters using mutation
+export const useFilteredOrderForecast = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: getFilteredOrderForecast,
+    onSuccess: (data: OrderForecast[]) => {
+      queryClient.setQueryData([QUERY_KEY], data);
+    },
+  });
+};
