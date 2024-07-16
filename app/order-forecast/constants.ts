a0aabfff912f2a0c3bd2df2e54dcd8aea411cb7a
krlsdgzmn <johnkarlos.deguzman.d@gmail.com>
2024-07-16 14:50:03 +0800

    + Added constants for order forecast

---
+++ app/order-forecast/constants.ts
@@ -0,0 +1,24 @@
+export const MONTH = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+export const WEEK = ["1", "2", "3", "4"];
+
+export const ORDER_STATUS = ["Completed", "Cancelled"];
+
+export const ORDER_FORECAST_FILTERS_INIT = {
+  month: [],
+  week: [],
+  order_status: [],
+};
