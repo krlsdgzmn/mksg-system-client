9e004e6963ddf7cc56b903febbf1b0ffa3cd78c7
krlsdgzmn <johnkarlos.deguzman.d@gmail.com>
2024-07-17 19:47:22 +0800

    🔧 Outlined the structure of dashboard panel

--- app/order-forecast/page.tsx
+++ app/order-forecast/page.tsx
@@ -1,7 +1,8 @@
 import Container from "@/components/container";
 import PageHeader from "@/components/page-header";
-import KPISection from "./_components/kpi-section";
 import DataFilters from "./_components/data-filters";
+import DistributionChart from "./_components/distribution-chart";
+import KPISection from "./_components/kpi-section";
 
 export default function OrderForecastingDashboardPage() {
   return (
@@ -12,7 +13,17 @@ export default function OrderForecastingDashboardPage() {
         filters={<DataFilters />}
       />
 
-      <KPISection />
+      <main className="grid gap-2 pt-2 xl:grid-cols-8 xl:gap-4 xl:pt-4">
+        {/* Left side */}
+        <section className="xl:col-span-5">
+          <KPISection />
+        </section>
+
+        {/* Right side */}
+        <section className="xl:col-span-3">
+          <DistributionChart />
+        </section>
+      </main>
     </Container>
   );
 }
