1d551f74c2852d8e0dc7351e93cd60a9fddedb33
krlsdgzmn <johnkarlos.deguzman.d@gmail.com>
2024-08-22 16:54:59 +0800

    🔧 Fixed header padding

--- components/header.tsx
+++ components/header.tsx
@@ -27,7 +27,7 @@ export default function Header() {
 
   return (
     <header className="top-0 z-50 w-full border-b border-border bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 md:sticky">
-      <div className="container flex h-14 max-w-screen-2xl items-center justify-between">
+      <div className="container flex h-14 max-w-screen-2xl items-center justify-between px-4 md:px-8">
         <div className="flex items-center justify-center gap-3">
           {/* logo */}
           <Image
