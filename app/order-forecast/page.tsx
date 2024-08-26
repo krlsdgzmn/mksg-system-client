baede0d6a8fa3dba1e71efd4132caa3a6830346e
krlsdgzmn <johnkarlos.deguzman.d@gmail.com>
2024-08-26 10:44:27 +0800

    🔧 Fixed rendering

--- app/order-forecast/page.tsx
+++ app/order-forecast/page.tsx
@@ -12,10 +12,12 @@ import MonthChart from "./_components/month-chart";
 import OrderStatusTable from "./_components/order-status-table";
 import WeekChart from "./_components/week-chart";
 import { useGetOrderForecast } from "./hooks";
+import { useAuth } from "../providers";
 
 export default function OrderForecastingDashboard() {
   const { data, isLoading, isError } = useGetOrderForecast();
   const { toast } = useToast();
+  const { data: user } = useAuth();
 
   const totalOrders = data ? data.length : 0;
   const completedOrders = data
@@ -35,39 +37,40 @@ export default function OrderForecastingDashboard() {
     }
   }, [isError, toast]);
 
-  return (
-    <Container className="flex min-h-[86vh] flex-col items-center overflow-auto">
-      <main className="w-full">
-        <PageHeader
-          header="Order Forecasting Dashboard"
-          subheader="Forecasts for Order Management and Fulfillment"
-          filters={<DataFilters />}
-        />
-        <div className="grid w-full gap-1 py-2 xl:grid-cols-8 xl:gap-4 xl:py-4">
-          {/* Left side */}
-          <section className="space-y-1 xl:col-span-5 xl:space-y-4">
-            <KPISection
-              totalOrders={totalOrders}
-              completedOrders={completedOrders}
-              cancelledOrders={cancelledOrders}
-            />
-            <MonthChart data={data} isLoading={isLoading} />
-            <OrderStatusTable data={data} isLoading={isLoading} />
-          </section>
+  if (user)
+    return (
+      <Container className="flex min-h-[86vh] flex-col items-center overflow-auto">
+        <main className="w-full">
+          <PageHeader
+            header="Order Forecasting Dashboard"
+            subheader="Forecasts for Order Management and Fulfillment"
+            filters={<DataFilters />}
+          />
+          <div className="grid w-full gap-1 py-2 xl:grid-cols-8 xl:gap-4 xl:py-4">
+            {/* Left side */}
+            <section className="space-y-1 xl:col-span-5 xl:space-y-4">
+              <KPISection
+                totalOrders={totalOrders}
+                completedOrders={completedOrders}
+                cancelledOrders={cancelledOrders}
+              />
+              <MonthChart data={data} isLoading={isLoading} />
+              <OrderStatusTable data={data} isLoading={isLoading} />
+            </section>
 
-          {/* Right side */}
-          <section className="space-y-1 xl:col-span-3 xl:space-y-4">
-            <DistributionChart
-              totalOrders={totalOrders}
-              completedOrders={completedOrders}
-              cancelledOrders={cancelledOrders}
-              isLoading={isLoading}
-            />
-            <DistanceChart data={data} isLoading={isLoading} />
-            <WeekChart data={data} isLoading={isLoading} />
-          </section>
-        </div>
-      </main>
-    </Container>
-  );
+            {/* Right side */}
+            <section className="space-y-1 xl:col-span-3 xl:space-y-4">
+              <DistributionChart
+                totalOrders={totalOrders}
+                completedOrders={completedOrders}
+                cancelledOrders={cancelledOrders}
+                isLoading={isLoading}
+              />
+              <DistanceChart data={data} isLoading={isLoading} />
+              <WeekChart data={data} isLoading={isLoading} />
+            </section>
+          </div>
+        </main>
+      </Container>
+    );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -12,8 +12,9 @@ import {
 import { Input } from "@/components/ui/input";
 import { useToast } from "@/components/ui/use-toast";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { LogIn } from "lucide-react";
+import { Loader2, LogIn } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { useAuth } from "./providers";
@@ -53,7 +54,8 @@ const inputs = [
 export default function SignInPage() {
   const { toast } = useToast();
   const router = useRouter();
-  const { signIn } = useAuth();
+  const { signIn, data } = useAuth();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const form = useForm<z.infer<typeof SignInFormSchema>>({
     resolver: zodResolver(SignInFormSchema),
@@ -64,6 +66,8 @@ export default function SignInPage() {
   });
 
   const onSubmit = async (values: z.infer<typeof SignInFormSchema>) => {
+    setIsLoading(true);
+
     try {
       await signIn(values.username, values.password);
       toast({
@@ -79,60 +83,72 @@ export default function SignInPage() {
         description: "Please try again.",
         variant: "destructive",
       });
+    } finally {
+      setIsLoading(false);
     }
   };
 
   return (
     <Container className="relative flex min-h-[85vh] items-center justify-center">
-      <div className="flex w-[95%] max-w-[300px] flex-col items-center justify-center gap-3">
-        <header className="pb-2 text-center">
-          <h1 className="bg-gradient-to-b from-black/60 to-black bg-clip-text text-3xl font-bold text-transparent dark:from-white dark:to-white/50">
-            MKSG Clothing
-          </h1>
-          <p className="text-base text-muted-foreground">
-            Please sign in to continue
-          </p>
-        </header>
+      {!data && (
+        <div className="flex w-[95%] max-w-[300px] flex-col items-center justify-center gap-3">
+          <header className="pb-2 text-center">
+            <h1 className="bg-gradient-to-b from-black/60 to-black bg-clip-text text-3xl font-bold text-transparent dark:from-white dark:to-white/50">
+              MKSG Clothing
+            </h1>
+            <p className="text-base text-muted-foreground">
+              Please sign in to continue
+            </p>
+          </header>
 
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(onSubmit)}
-            className="w-full space-y-3"
-          >
-            {inputs.map((item) => (
-              <FormField
-                key={item.name}
-                control={form.control}
-                name={
-                  item.name.toLowerCase() as keyof z.infer<
-                    typeof SignInFormSchema
-                  >
-                }
-                render={({ field }) => (
-                  <FormItem>
-                    <FormControl>
-                      <Input
-                        type={item.type}
-                        placeholder={item.name}
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage className="text-sm text-red-500/90" />
-                  </FormItem>
-                )}
-              />
-            ))}
-
-            <Button
-              className="flex w-full items-center gap-1 text-sm"
-              variant="outline"
-              type="submit"
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="w-full space-y-3"
             >
-              Sign In <LogIn size={14} />
-            </Button>
-          </form>
-        </Form>
-      </div>
+              {inputs.map((item) => (
+                <FormField
+                  key={item.name}
+                  control={form.control}
+                  name={
+                    item.name.toLowerCase() as keyof z.infer<
+                      typeof SignInFormSchema
+                    >
+                  }
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <Input
+                          type={item.type}
+                          placeholder={item.name}
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage className="text-sm text-red-500/90" />
+                    </FormItem>
+                  )}
+                />
+              ))}
+
+              <Button
+                className="flex w-full items-center gap-1 text-sm"
+                variant="outline"
+                type="submit"
+                disabled={isLoading}
+              >
+                {isLoading && <Loader2 className="h-4 w-4 animate-spin" />}
+                {isLoading ? (
+                  "Signing In..."
+                ) : (
+                  <>
+                    Sign In <LogIn size={14} />
+                  </>
+                )}
+              </Button>
+            </form>
+          </Form>
+        </div>
+      )}
     </Container>
   );
 }
