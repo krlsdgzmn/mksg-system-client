ab82fba224c0ec8c3ee0b0e5b7d7bccdf5ad0f89
krlsdgzmn <johnkarlos.deguzman.d@gmail.com>
2024-08-30 16:48:13 +0800

    🔧 Fixed rendering

--- app/page.tsx
+++ app/page.tsx
@@ -60,7 +60,7 @@ const inputs = [
 export default function SignInPage() {
   const { toast } = useToast();
   const router = useRouter();
-  const { signIn } = useAuth();
+  const { signIn, data } = useAuth();
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const form = useForm<z.infer<typeof SignInFormSchema>>({
@@ -94,72 +94,66 @@ export default function SignInPage() {
     }
   };
 
-  if (isLoading)
+  if (!data)
     return (
-      <Container className="relative flex min-h-screen items-center justify-center border">
-        <Loader2 size={32} className="animate-spin" />
+      <Container className="relative flex min-h-[85vh] items-center justify-center">
+        <div className="flex w-[95%] max-w-[300px] flex-col items-center justify-center gap-3">
+          <header className="pb-2 text-center">
+            <h1 className="bg-gradient-to-b from-black/60 to-black bg-clip-text text-3xl font-bold text-transparent dark:from-white dark:to-white/50">
+              MKSG Clothing
+            </h1>
+            <p className="text-base text-muted-foreground">
+              Please sign in to continue
+            </p>
+          </header>
+
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="w-full space-y-3"
+            >
+              {inputs.map((item) => (
+                <FormField
+                  key={item.name}
+                  control={form.control}
+                  name={
+                    item.name.toLowerCase() as keyof z.infer<
+                      typeof SignInFormSchema
+                    >
+                  }
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <Input
+                          type={item.type}
+                          placeholder={item.name}
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage className="text-sm text-red-500/90" />
+                    </FormItem>
+                  )}
+                />
+              ))}
+
+              <Button
+                className="flex w-full items-center gap-1 text-sm"
+                variant="outline"
+                type="submit"
+                disabled={isLoading}
+              >
+                {isLoading && <Loader2 className="h-4 w-4 animate-spin" />}
+                {isLoading ? (
+                  "Signing In..."
+                ) : (
+                  <>
+                    Sign In <LogIn size={14} />
+                  </>
+                )}
+              </Button>
+            </form>
+          </Form>
+        </div>
       </Container>
     );
-
-  return (
-    <Container className="relative flex min-h-[85vh] items-center justify-center">
-      <div className="flex w-[95%] max-w-[300px] flex-col items-center justify-center gap-3">
-        <header className="pb-2 text-center">
-          <h1 className="bg-gradient-to-b from-black/60 to-black bg-clip-text text-3xl font-bold text-transparent dark:from-white dark:to-white/50">
-            MKSG Clothing
-          </h1>
-          <p className="text-base text-muted-foreground">
-            Please sign in to continue
-          </p>
-        </header>
-
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(onSubmit)}
-            className="w-full space-y-3"
-          >
-            {inputs.map((item) => (
-              <FormField
-                key={item.name}
-                control={form.control}
-                name={
-                  item.name.toLowerCase() as keyof z.infer<
-                    typeof SignInFormSchema
-                  >
-                }
-                render={({ field }) => (
-                  <FormItem>
-                    <FormControl>
-                      <Input
-                        type={item.type}
-                        placeholder={item.name}
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage className="text-sm text-red-500/90" />
-                  </FormItem>
-                )}
-              />
-            ))}
-
-            <Button
-              className="flex w-full items-center gap-1 text-sm"
-              variant="outline"
-              type="submit"
-              disabled={isLoading}
-            >
-              {isLoading && <Loader2 className="h-4 w-4 animate-spin" />}
-              {isLoading ? (
-                "Signing In..."
-              ) : (
-                <>
-                  Sign In <LogIn size={14} />
-                </>
-              )}
-            </Button>
-          </form>
-        </Form>
-      </div>
-    </Container>
-  );
 }
