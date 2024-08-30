e80ed394aa8cb56402a2a51dfe6791f9b3e23491
krlsdgzmn <johnkarlos.deguzman.d@gmail.com>
2024-08-30 18:34:33 +0800

    🔧 Fixed border

--- components/loader.tsx
+++ components/loader.tsx
@@ -3,7 +3,7 @@ import Container from "./container";
 
 export default function Loader() {
   return (
-    <Container className="relative flex min-h-[86vh] items-center justify-center border">
+    <Container className="relative flex min-h-[86vh] items-center justify-center">
       <Loader2 size={32} className="animate-spin" />
     </Container>
   );
