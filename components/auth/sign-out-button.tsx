f4329f253107db5a7d3cecf3de02322bae10161d
krlsdgzmn <johnkarlos.deguzman.d@gmail.com>
2024-10-01 19:59:56 +0800

    feat: add sign out toaster message

--- components/auth/sign-out-button.tsx
+++ components/auth/sign-out-button.tsx
@@ -1,17 +1,36 @@
 "use client";
 
 import { signOutAction } from "@/actions/auth-actions";
+import { useToast } from "@/hooks/use-toast";
 import { LogOut } from "lucide-react";
+import { useState } from "react";
 import { Button } from "../ui/button";
 
 export default function SignOutButton() {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const { toast } = useToast();
+
+  const handleSignOut = async () => {
+    setIsLoading(true);
+    try {
+      await signOutAction();
+      toast({
+        title: "Success",
+        description: "You have successfully signed out.",
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <Button
       title="Sign out"
       variant="ghost"
       size="icon"
       className="rounded-full"
-      onClick={async () => await signOutAction()}
+      onClick={handleSignOut}
+      disabled={isLoading}
     >
       <LogOut size={18} />
     </Button>
