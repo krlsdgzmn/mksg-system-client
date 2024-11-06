b7530c00e7d0b7424a028a5e1cddf1bf0861c6da
krlsdgzmn <johnkarlos.deguzman.d@gmail.com>
2024-11-06 22:38:38 +0800

    fix: fix footer alignment

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="top-0 z-50 w-full border-t border-border bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 md:sticky">
+    <footer className="top-0 z-50 flex w-full items-center justify-center border-t border-border bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 md:sticky">
       <div className="container flex h-14 max-w-screen-2xl items-center justify-center">
         <h2 className="text-center text-sm text-foreground/40">
           &copy; {new Date().getFullYear()} All rights reserved.
