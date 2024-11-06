a5ae46ca95b136cc36d158fd2d1e54945a6c6f11
krlsdgzmn <johnkarlos.deguzman.d@gmail.com>
2024-11-06 14:16:16 +0800

    feat: add patttern forecasts filter

--- components/visitor-forecast/forecasts-pattern-dashboard.tsx
+++ components/visitor-forecast/forecasts-pattern-dashboard.tsx
@@ -1,8 +1,62 @@
 "use client";
 
+import { SlidersHorizontal } from "lucide-react";
+import { Button } from "../ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
 import ForecastsPatternChart from "./forecasts-pattern-chart";
+import { useState } from "react";
+import {
+  ForecastsPattern,
+  forecastsPatternData,
+} from "@/constants/forecasts-pattern";
+import { Checkbox } from "../ui/checkbox";
+
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 
 export default function ForecastsPatternDashboard() {
+  // Initialize filters to include all months by default
+  const [filters, setFilters] = useState<string[]>(months);
+
+  const handleClearFilters = () => {
+    setFilters([]);
+  };
+
+  const handleFilterChange = (month: string, isChecked: boolean) => {
+    setFilters((prevFilters) =>
+      isChecked
+        ? [...prevFilters, month]
+        : prevFilters.filter((m) => m !== month),
+    );
+  };
+
+  // Filter data based on selected months
+  const filteredData = forecastsPatternData.map((entry) => {
+    const filteredEntry: ForecastsPattern = { hour: entry.hour };
+    months.forEach((month) => {
+      filteredEntry[month] = filters.includes(month) ? entry[month] : 0;
+    });
+    return filteredEntry;
+  });
+
   return (
     <main className="space-y-2 py-2 xl:space-y-4 xl:py-4">
       <div className="min-h-[600px] overflow-hidden rounded-md border border-border bg-card p-4 shadow dark:bg-muted-foreground/10">
@@ -15,9 +69,56 @@ export default function ForecastsPatternDashboard() {
               Average Hourly Page Views by Month
             </h3>
           </div>
+
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline" className="flex items-center gap-2">
+                <SlidersHorizontal size={14} /> Filters
+              </Button>
+            </DropdownMenuTrigger>
+
+            <DropdownMenuContent
+              className="ml-8 w-64 bg-card/95 backdrop-blur supports-[backdrop-filter]:bg-background/90"
+              align="end"
+            >
+              <DropdownMenuLabel className="flex items-center justify-between py-0">
+                <span>Filters</span>
+                <Button
+                  variant="link"
+                  className="p-0"
+                  onClick={handleClearFilters}
+                >
+                  Clear
+                </Button>
+              </DropdownMenuLabel>
+
+              <DropdownMenuSeparator className="mx-0.5 bg-border" />
+
+              <h1 className="ml-2 text-sm font-medium">Month</h1>
+              <div className="mx-2 grid w-full grid-cols-2 gap-y-2 py-2">
+                {months.map((month) => (
+                  <div className="flex items-center gap-1.5" key={month}>
+                    <Checkbox
+                      id={month}
+                      checked={filters.includes(month)}
+                      onCheckedChange={(isChecked) =>
+                        handleFilterChange(month, isChecked)
+                      }
+                    />
+                    <label
+                      htmlFor={month}
+                      className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                    >
+                      {month}
+                    </label>
+                  </div>
+                ))}
+              </div>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </header>
 
-        <ForecastsPatternChart />
+        <ForecastsPatternChart data={filteredData} />
       </div>
     </main>
   );
