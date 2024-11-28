597420ea08052c0eb14f5d917132a61442732291
krlsdgzmn <johnkarlos.deguzman.d@gmail.com>
2024-11-29 03:00:44 +0800

    feat: add fill

--- components/visitor-forecast/forecasts-pattern-chart.tsx
+++ components/visitor-forecast/forecasts-pattern-chart.tsx
@@ -99,19 +99,78 @@ export default function ForecastsPatternChart({
           content={<ChartTooltipContent labelFormatter={(_) => ""} />}
         />
 
-        {/* Define a Line for each month */}
-        <Line type="linear" dataKey="January" stroke="#8886d8" />
-        <Line type="linear" dataKey="February" stroke="#84ca9d" />
-        <Line type="linear" dataKey="March" stroke="#ffc660" />
-        <Line type="linear" dataKey="April" stroke="#ff7302" />
-        <Line type="linear" dataKey="May" stroke="#387910" />
-        <Line type="linear" dataKey="June" stroke="#8886d8" />
-        <Line type="linear" dataKey="July" stroke="#ff71b4" />
-        <Line type="linear" dataKey="August" stroke="#ba70c8" />
-        <Line type="linear" dataKey="September" stroke="#6dd0e1" />
-        <Line type="linear" dataKey="October" stroke="#d34f2f" />
-        <Line type="linear" dataKey="November" stroke="#9b1fa2" />
-        <Line type="linear" dataKey="December" stroke="#05a9f4" />
+        <Line
+          type="linear"
+          dataKey="January"
+          stroke="#8886d8"
+          dot={{ fill: "#8886d8", r: 3 }}
+        />
+        <Line
+          type="linear"
+          dataKey="February"
+          stroke="#84ca9d"
+          dot={{ fill: "#84ca9d", r: 3 }}
+        />
+        <Line
+          type="linear"
+          dataKey="March"
+          stroke="#ffc660"
+          dot={{ fill: "#ffc660", r: 3 }}
+        />
+        <Line
+          type="linear"
+          dataKey="April"
+          stroke="#ff7302"
+          dot={{ fill: "#ff7302", r: 3 }}
+        />
+        <Line
+          type="linear"
+          dataKey="May"
+          stroke="#387910"
+          dot={{ fill: "#387910", r: 3 }}
+        />
+        <Line
+          type="linear"
+          dataKey="June"
+          stroke="#8886d8"
+          dot={{ fill: "#8886d8", r: 3 }}
+        />
+        <Line
+          type="linear"
+          dataKey="July"
+          stroke="#ff71b4"
+          dot={{ fill: "#ff71b4", r: 3 }}
+        />
+        <Line
+          type="linear"
+          dataKey="August"
+          stroke="#ba70c8"
+          dot={{ fill: "#ba70c8", r: 3 }}
+        />
+        <Line
+          type="linear"
+          dataKey="September"
+          stroke="#6dd0e1"
+          dot={{ fill: "#6dd0e1", r: 3 }}
+        />
+        <Line
+          type="linear"
+          dataKey="October"
+          stroke="#d34f2f"
+          dot={{ fill: "#d34f2f", r: 3 }}
+        />
+        <Line
+          type="linear"
+          dataKey="November"
+          stroke="#9b1fa2"
+          dot={{ fill: "#9b1fa2", r: 3 }}
+        />
+        <Line
+          type="linear"
+          dataKey="December"
+          stroke="#05a9f4"
+          dot={{ fill: "#05a9f4", r: 3 }}
+        />
       </ComposedChart>
     </ChartContainer>
   );
